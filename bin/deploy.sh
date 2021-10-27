e6865bb64ab9f0173aaa1f99894a416ec80bb75d
Yannick Rehberger <yr@ityreh.de>
2021-10-27 17:40:56 +0200

    fix(deploy): rename config-file path

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../../ityreh.github.io
-mkdocs gh-deploy --config-file ../material-docs/mkdocs.yml --remote-branch main
+mkdocs gh-deploy --config-file ../mkdocs/mkdocs.yml --remote-branch main
